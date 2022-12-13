761bd63482ef32160e6eda7b142ee465730df625
Frida Gidlund <frida_gidde95@hotmail.com>
2022-12-13 23:19:00 +0100

    Added some barebones unittests

--- test/Arbetsprov.CSharp.Web.Tests/Services/WeatherServiceTests.cs
+++ test/Arbetsprov.CSharp.Web.Tests/Services/WeatherServiceTests.cs
@@ -1,15 +1,42 @@
-//using Arbetsprov.CSharp.Web.Services;
+using Arbetsprov.CSharp.Web.Contracts;
+using Arbetsprov.CSharp.Web.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Arbetsprov.CSharp.Web.Tests.Services
 {
-    public class WeatherServiceTests
-    {
-        [Fact]
-        public void CanConstruct()
-        {
-            //var service = new WeatherService();
-            //Assert.NotNull(service);
-        }
-    }
+	public class WeatherServiceTests
+	{
+		private readonly IWeatherService _weatherService;
+
+		// todo DI
+		public WeatherServiceTests()
+		{
+			var services = new ServiceCollection();
+			services.AddTransient<IWeatherService, WeatherService>();
+			var serviceProvider = services.BuildServiceProvider();
+			_weatherService = serviceProvider.GetService<IWeatherService>();
+		}
+
+		[Fact]
+		public void CanConstruct()
+		{
+			//var service = new WeatherService();
+			Assert.NotNull(_weatherService);
+		}
+
+		[Fact]
+		public async void CallWeatherAPI()
+		{
+			var forecast = await _weatherService.GetLocationForecastAsync(63.8258, 20.2630, new System.Threading.CancellationToken());
+			Assert.NotNull(forecast);
+		}
+
+		[Fact]
+		public async void CallWeatherAPINull()
+		{
+			var nowhere = await _weatherService.GetLocationForecastAsync(0, 0, new System.Threading.CancellationToken());
+			Assert.Null(nowhere);
+		}
+	}
 }
